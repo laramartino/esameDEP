b1044e90a91918343dfd78980e0bf6d794351ef1
Lara Martino <martinolara@gmail.com>
2025-08-31 17:51:15 +0200

    -

--- DEPgraphql/resource-service/app/main.py
+++ DEPgraphql/resource-service/app/main.py
@@ -144,76 +144,67 @@ class Mutation:
     def add_piscina(self, booking: PiscinaBookingInput) -> BookingResponse:
         db: Session = next(get_db())
 
-        try:
-            cf = booking.cf.upper()
-            if not check_member(cf):
-                raise Exception("CF doesn't exist")
-            if booking.data <= date.today():
-                raise Exception("La data deve essere successiva ad oggi")
+        cf = booking.cf.upper()
+        if not check_member(cf):
+            raise Exception("CF doesn't exist")
+        if booking.data <= date.today():
+            raise Exception("La data deve essere successiva ad oggi")
 
-            mese, giorno = booking.data.month, booking.data.day
-            inizio = (5, 20)  # 20 maggio
-            fine = (9, 15)  # 15 settembre
-            if not (inizio <= (mese, giorno) <= fine):
-                raise Exception("La data deve essere compresa tra il 20 maggio e il 15 settembre")
+        existing = db.query(PrenotazioniPiscina).filter_by(
+            data=booking.data,
+            cf=cf).first()
+        if existing:
+            raise Exception(f"{cf} has already a reservation")
 
-            lettini_prenotati = db.query(func.sum(PrenotazioniPiscina.lettini)).filter(
-                PrenotazioniPiscina.data == booking.data
-            ).scalar() or 0
-            if lettini_prenotati + booking.lettini > 80:
-                lettini_disponibili = 80 - lettini_prenotati
-                raise Exception(f"Only {lettini_disponibili} lettini available on {booking.data}")
+        mese, giorno = booking.data.month, booking.data.day
+        inizio = (5, 20)  # 20 maggio
+        fine = (9, 15)  # 15 settembre
+        if not (inizio <= (mese, giorno) <= fine):
+            raise Exception("La data deve essere compresa tra il 20 maggio e il 15 settembre")
 
-            ombrelloni_prenotati = db.query(func.sum(PrenotazioniPiscina.ombrelloni)).filter(
-                PrenotazioniPiscina.data == booking.data
-            ).scalar() or 0
-            if ombrelloni_prenotati + booking.ombrelloni > 20:
-                ombrelloni_disponibili = 20 - ombrelloni_prenotati
-                raise Exception(f"Only {ombrelloni_disponibili} ombrelloni available on {booking.data}")
+        lettini_prenotati = db.query(func.sum(PrenotazioniPiscina.lettini)).filter(
+                                                PrenotazioniPiscina.data == booking.data).scalar() or 0
+        if lettini_prenotati + booking.lettini > 80:
+            lettini_disponibili = 80 - lettini_prenotati
+            raise Exception(f"Only {lettini_disponibili} lettini available on {booking.data}")
 
-            existing = db.query(PrenotazioniPiscina).filter_by(
-                data=booking.data,
-                cf=cf
-            ).first()
-            if existing:
-                raise Exception(f"{cf} has already a reservation")
+        ombrelloni_prenotati = db.query(func.sum(PrenotazioniPiscina.ombrelloni)).filter(
+                                                    PrenotazioniPiscina.data == booking.data).scalar() or 0
+        if ombrelloni_prenotati + booking.ombrelloni > 20:
+            ombrelloni_disponibili = 20 - ombrelloni_prenotati
+            raise Exception(f"Only {ombrelloni_disponibili} ombrelloni available on {booking.data}")
 
-            new = PrenotazioniPiscina(
+        new = PrenotazioniPiscina(
                 cf=cf,
                 data=booking.data,
                 lettini=booking.lettini,
-                ombrelloni=booking.ombrelloni
-            )
-            db.add(new)
-            db.commit()
-            return BookingResponse(detail="Booking added")
-        finally:
-            db.close()
+                ombrelloni=booking.ombrelloni)
+        db.add(new)
+        db.commit()
+        db.close()
+        return BookingResponse(detail="Booking added")
 
     @strawberry.mutation
-    def delete_piscina(self, booking: PiscinaBookingInput) -> BookingResponse:
+    def delete_piscina(self, cf: str, data: date) -> BookingResponse:
         db: Session = next(get_db())
 
-        try:
-            prenotazione = db.query(PrenotazioniPiscina).filter_by(
-                cf=booking.cf.upper(),
-                data=booking.data,
-                lettini=booking.lettini,
-                ombrelloni=booking.ombrelloni
-            ).first()
-            if prenotazione:
-                db.delete(prenotazione)
-                db.commit()
-                return BookingResponse(detail="Booking deleted")
-            raise Exception("Booking not found")
-        finally:
+        prenotazione = db.query(PrenotazioniPiscina).filter_by(
+                cf=cf.upper(),
+                data=data).first()
+        if prenotazione:
+            db.delete(prenotazione)
+            db.commit()
             db.close()
+            return BookingResponse(detail="Booking deleted")
+        raise Exception("Booking not found")
 
     @strawberry.mutation
     def delete_prenotazioni(self, cf: str) -> None:
         db: Session = next(get_db())
-        db.query(PrenotazioniCampi).filter(PrenotazioniCampi.cf == cf).delete(synchronize_session=False)
-        db.query(PrenotazioniPiscina).filter(PrenotazioniPiscina.cf == cf).delete(synchronize_session=False)
+        db.query(PrenotazioniCampi).filter(PrenotazioniCampi.cf == cf,
+                                           PrenotazioniCampi.data >= date.today()).delete(synchronize_session=False)
+        db.query(PrenotazioniPiscina).filter(PrenotazioniPiscina.cf == cf,
+                                             PrenotazioniPiscina.data >= date.today()).delete(synchronize_session=False)
         db.commit()
         db.close()
         return
